7a924a6cad04ec62de3df4d3cbf3520fe8d434ba
Jason_song97 <jaegeunsong97@gmail.com>
2023-10-24 00:50:11 +0900

    KAN-26 FEAT: 구글 기본 구성완료

--- auth/src/controller/google.controller.ts
+++ auth/src/controller/google.controller.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import * as JWT from '../utils/jwt.utils';
+import * as crypto from 'crypto';
+import { CustomError } from "../utils/api.utils";
 
 export const googleAuthentication = async (code: string): Promise<string> => {
      const clientId: string = process.env.GOOGLE_CLIENTID as string;
@@ -7,6 +9,9 @@ export const googleAuthentication = async (code: string): Promise<string> => {
      const GOOGLE_TOKEN_URL: string = process.env.GOOGLE_TOKEN_URL as string;
      const GOOGLE_USERINFO_URL: string = process.env.GOOGLE_USERINFO_URL as string;
      const REDIRECT_URI: string = process.env.REDIRECT_URI as string;
+
+     const sha256Hash = crypto.createHash('sha256');
+     const tempPwd = "1234";
 
      const { data } = await axios.post(GOOGLE_TOKEN_URL, {
           grant_type: 'authorization_code',
@@ -19,9 +24,29 @@ export const googleAuthentication = async (code: string): Promise<string> => {
      const userInfoRequest = await axios.get(GOOGLE_USERINFO_URL, {
           headers: {
                Authorization: `Bearer ${accessToken}`,
-           },
+          },
      });
      console.log(userInfoRequest.data);
-     const token: string = await JWT.generateToken(userInfoRequest.data);
+
+     let payLoad: JWT.Payload = {
+          id: userInfoRequest.data.id,
+          email: userInfoRequest.data.email,
+          password: sha256Hash.update(tempPwd).digest('hex'),
+          thumbnail: userInfoRequest.data.picture,
+          loginType: 'GOOGLE'
+     }
+     payLoad = handleNullCheck(payLoad);
+
+     const token: string = await JWT.generateToken(payLoad);
      return token;
 }
+
+const handleNullCheck = (payLoad: JWT.Payload): JWT.Payload => {
+     return {
+         id : payLoad.id !== null ? payLoad.id : 'NONE',
+         email : payLoad.email !== null ? payLoad.email : 'NONE',
+         password : payLoad.password !== null ? payLoad.password : 'NONE',
+         thumbnail : payLoad.thumbnail !== null ? payLoad.thumbnail : 'NONE', 
+         loginType: 'GOOGLE'
+     };
+ }
