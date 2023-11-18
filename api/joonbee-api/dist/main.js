72215dd8fb9e9c5cab9c4cf28e46bd6aa803db18
Jason_song97 <jaegeunsong97@gmail.com>
2023-11-18 20:25:05 +0900

    joonbee.sql 업데이트

--- api/joonbee-api/dist/main.js
+++ api/joonbee-api/dist/main.js
@@ -18,7 +18,7 @@ async function bootstrap() {
         .addTag('joonbee')
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
-    swagger_1.SwaggerModule.setup('api', app, document);
+    swagger_1.SwaggerModule.setup('api/swagger', app, document);
     await app.listen(3030);
 }
 bootstrap();
--- joonbee.sql
+++ joonbee.sql
@@ -7,36 +7,6 @@ CREATE TABLE IF NOT EXISTS `category` (
     `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS `member` (
-    id varchar(255) PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    thumbnail TEXT,
-    login_type varchar(50),
-    del_flag boolean not null default false,
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-CREATE TABLE `interview` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `member_id` varchar(255) NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `category_name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `member_id` (`member_id`),
-  CONSTRAINT `interview_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-
-CREATE TABLE IF NOT EXISTS `like` (
-	`member_id` varchar(255) not null,
-	`interview_id` BIGINT not null,
-	 primary key (`member_id`, `interview_id`),
-	 foreign key (`member_id`) REFERENCES `member`(`id`) on delete cascade,
-	 foreign key (`interview_id`) REFERENCES `interview`(`id`) on delete CASCADE
-)ENGINE=InnoDB;
-
 CREATE TABLE IF NOT EXISTS `question` (
     `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
     `gpt_flag` tinyint(1) NOT NULL,
@@ -48,6 +18,37 @@ CREATE TABLE IF NOT EXISTS `question` (
     `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     foreign key(`category_id`) REFERENCES `category`(`id`) on delete cascade
 ) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `member` (
+    id varchar(255) PRIMARY KEY,
+    nick_name varchar(255) not null,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    thumbnail TEXT,
+    login_type varchar(50),
+    del_flag boolean not null default false,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `interview` (
+  `id` bigint AUTO_INCREMENT,
+  `member_id` varchar(255) NOT NULL,
+  `category_name` varchar(255) not null,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `interview_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `like` (
+	`member_id` varchar(255) not null,
+	`interview_id` BIGINT not null,
+	 primary key (`member_id`, `interview_id`),
+	 foreign key (`member_id`) REFERENCES `member`(`id`) on delete cascade,
+	 foreign key (`interview_id`) REFERENCES `interview`(`id`) on delete CASCADE
+)ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `interview_and_question` (
   `interview_id` bigint NOT NULL,
