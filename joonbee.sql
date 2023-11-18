762276cc65dda9d02beb8e43c344c5b71278c48b
Jason_song97 <jaegeunsong97@gmail.com>
2023-11-18 20:41:13 +0900

    joonbee.sql 수정

--- joonbee.sql
+++ joonbee.sql
@@ -1,61 +1,66 @@
-CREATE TABLE IF NOT EXISTS `category` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `category_name` VARCHAR(255) NOT NULL,
-    `category_level` tinyint(1) NOT NULL,
-    `category_upper_id` INT NOT NULL,
-    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB;
+CREATE TABLE `category` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `category_name` varchar(255) NOT NULL,
+   `category_level` tinyint(1) NOT NULL,
+   `category_upper_id` int(11) NOT NULL,
+   `created_at` datetime DEFAULT current_timestamp(),
+   `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+   PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
 
-CREATE TABLE IF NOT EXISTS `question` (
-    `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `gpt_flag` tinyint(1) NOT NULL,
-    `question_level` INT NOT NULL,
-    `category_id` int not null,
-    `writer` VARCHAR(255) NOT NULL,
-    `question_content` TEXT NOT NULL,
-    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    foreign key(`category_id`) REFERENCES `category`(`id`) on delete cascade
-) ENGINE=InnoDB;
+CREATE TABLE `question` (
+   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+   `gpt_flag` tinyint(1) NOT NULL,
+   `question_level` int(11) NOT NULL,
+   `category_id` int(11) NOT NULL,
+   `writer` varchar(255) NOT NULL,
+   `question_content` text NOT NULL,
+   `created_at` datetime DEFAULT current_timestamp(),
+   `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+   PRIMARY KEY (`id`),
+   KEY `category_id` (`category_id`),
+   CONSTRAINT `question_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE
+ ) ENGINE=InnoDB AUTO_INCREMENT=4654 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
 
-CREATE TABLE IF NOT EXISTS `member` (
-    id varchar(255) PRIMARY KEY,
-    nick_name varchar(255) not null,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    thumbnail TEXT,
-    login_type varchar(50),
-    del_flag boolean not null default false,
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+CREATE TABLE `member` (
+   `id` varchar(255) NOT NULL,
+   `nick_name` varchar(255) NOT NULL,
+   `email` varchar(255) NOT NULL,
+   `password` varchar(255) NOT NULL,
+   `thumbnail` text DEFAULT NULL,
+   `login_type` varchar(50) DEFAULT NULL,
+   `del_flag` tinyint(1) NOT NULL DEFAULT 0,
+   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+   PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
 
-CREATE TABLE IF NOT EXISTS `interview` (
-  `id` bigint AUTO_INCREMENT,
-  `member_id` varchar(255) NOT NULL,
-  `category_name` varchar(255) not null,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `member_id` (`member_id`),
-  CONSTRAINT `interview_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `interview` (
+   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+   `member_id` varchar(255) NOT NULL,
+   `category_name` varchar(255) NOT NULL,
+   `created_at` datetime DEFAULT current_timestamp(),
+   `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+   PRIMARY KEY (`id`),
+   KEY `member_id` (`member_id`),
+   CONSTRAINT `interview_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE
+ ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
 
-CREATE TABLE IF NOT EXISTS `like` (
-	`member_id` varchar(255) not null,
-	`interview_id` BIGINT not null,
-	 primary key (`member_id`, `interview_id`),
-	 foreign key (`member_id`) REFERENCES `member`(`id`) on delete cascade,
-	 foreign key (`interview_id`) REFERENCES `interview`(`id`) on delete CASCADE
-)ENGINE=InnoDB;
+CREATE TABLE `like` (
+   `member_id` varchar(255) NOT NULL,
+   `interview_id` bigint(20) NOT NULL,
+   PRIMARY KEY (`member_id`,`interview_id`),
+   KEY `interview_id` (`interview_id`),
+   CONSTRAINT `like_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE,
+   CONSTRAINT `like_ibfk_2` FOREIGN KEY (`interview_id`) REFERENCES `interview` (`id`) ON DELETE CASCADE
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
 
-CREATE TABLE IF NOT EXISTS `interview_and_question` (
-  `interview_id` bigint NOT NULL,
-  `question_id` bigint NOT NULL,
-  `answer_content` text not null,
-  PRIMARY KEY (`interview_id`,`question_id`),
-  KEY `question_id` (`question_id`),
-  CONSTRAINT `interview_and_question_ibfk_1` FOREIGN KEY (`interview_id`) REFERENCES `interview` (`id`),
-  CONSTRAINT `interview_and_question_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `interview_and_question` (
+   `interview_id` bigint(20) NOT NULL,
+   `question_id` bigint(20) NOT NULL,
+   `answer_content` text NOT NULL,
+   PRIMARY KEY (`interview_id`,`question_id`),
+   KEY `question_id` (`question_id`),
+   CONSTRAINT `interview_and_question_ibfk_1` FOREIGN KEY (`interview_id`) REFERENCES `interview` (`id`),
+   CONSTRAINT `interview_and_question_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
