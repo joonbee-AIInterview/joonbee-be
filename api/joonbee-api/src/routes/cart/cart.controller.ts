69e5d41590105eabfeed803f96afeb99e2ddfd88
Jason_song97 <jaegeunsong97@gmail.com>
2023-11-29 16:27:47 +0900

    KAN-70 FEAT: 사용자 장바구니 질문 하위 카테고리별 조회 구현

--- api/joonbee-api/src/routes/cart/cart.controller.ts
+++ api/joonbee-api/src/routes/cart/cart.controller.ts
@@ -41,4 +41,35 @@ export class CartController {
              throw new CustomError('알 수 없는 에러 : ' + error,500);
         }
      }
+
+     /**
+      * @api 사용자의 장바구니 질문을 하위 카테고리로 필터링해서 가져온다.
+      */
+     @UseGuards(TokenAuthGuard)
+     @Get('questions/subcategory')
+     async getMemberCartsBySubcategory(
+          @Query('page') page: string,
+          @Query('category') category: string,
+          @Query('subcategory') subcategory: string,
+          @Res() response: Response,
+     ) {
+          // 유효성 검사
+          if (page === "") throw new CustomError('페이지가 비었습니다. ', 400);
+          if (category === "") throw new CustomError('카테고리가 비었습니다. ', 400);
+          if (subcategory === "") throw new CustomError('서브카테고리가 비었습니다. ', 400);
+          // 0인 경우 1로 바꾸기
+          if (page === "0") page = "1";
+          const memberId = response.locals.memberId;
+
+          try {
+               const data = await this.cartService.getMemberCartsBySubcategory(Number(page), memberId, category, subcategory);
+               const apiResponse: ApiResponse<ResponseCartQuestionsDTO> = {
+                    status: 200,
+                    data
+               }
+               response.json(apiResponse);
+          } catch (error) {
+               throw new CustomError('알 수 없는 에러 : ' + error,500);
+          }
+     }
 }
--- api/joonbee-api/src/routes/cart/cart.service.ts
+++ api/joonbee-api/src/routes/cart/cart.service.ts
@@ -58,4 +58,46 @@ export class CartService {
                throw new CustomError('면접 전, 사용자의 장바구니 질문 데이터 전체 조회 실패', 500);
           }
      }
+
+     /**
+      * @note 사용자 장바구니에 있는 모든 질문들을 하위 카테고리별로 10개씩 가져온다.
+      */
+     async getMemberCartsBySubcategory(page: number, memberId: string, category: string, subcategory: string): Promise<ResponseCartQuestionsDTO> {
+          const skipNumber = (page - 1) * this.PAGE_SIZE;
+          try {
+               const countQuery: RowDataPacket = await this.cartRepository.createQueryBuilder('cart')
+                    .select('COUNT(*)', 'count')
+                    .where('cart.member_id = :member_id', { member_id: memberId })
+                    .andWhere('cart.category_name = :category_name', { category_name: subcategory })
+                    .getRawOne();
+
+               const rowPacket: RowDataPacket[] = await this.cartRepository.createQueryBuilder('cart')
+                    .select([
+                        'cart.question_id',
+                        '(SELECT category_name FROM category WHERE id = (SELECT category_upper_id FROM category WHERE category_name = cart.category_name)) AS category_name',
+                        'cart.category_name AS subcategory_name',
+                        'q.question_content'
+                    ])
+                    .innerJoin('question', 'q', 'cart.question_id = q.id')
+                    .where('cart.member_id = :member_id AND cart.category_name = :category_name', { member_id: memberId, category_name: subcategory })
+                    .offset(skipNumber)
+                    .limit(this.PAGE_SIZE)
+                    .getRawMany();
+
+               const cartQuestionsDTOs: ResponseCartQuestionsOfMemberData[] = rowPacket.map(packet => ({
+                    questionId: packet.question_id,
+                    category: packet.category_name,
+                    subcategory: packet.subcategory_name,
+                    questionContent: packet.question_content,
+               }));
+               const result: ResponseCartQuestionsDTO = {
+                    total: Number(countQuery.count),
+                    result: cartQuestionsDTOs
+               }
+               return result;
+          } catch (error) {
+               console.log('getMemberCartsBySubcategory ERROR cart.service 99\n' + error);
+               throw new CustomError('면접 전, 사용자의 장바구니 하위카테고리 필터 질문 데이터 전체 조회 실패', 500);
+          }
+     }
 }
